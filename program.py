d37b27782f8557f48ae65573ccf65d76525ef822
Dominik Alkhovik <dominik.alk@hotmail.com>
2022-04-19 11:54:26 +0100

    start querying document

--- program.py
+++ program.py
@@ -1,5 +1,6 @@
+from fileinput import lineno
 from os.path import exists
-import sys
+import sys, re
 
 def read_file_path():
     file_path = input("What is the path of the file?: ")
@@ -7,7 +8,7 @@ def read_file_path():
 
 def read_files(file_path):
     file = []
-    query = []
+    queries = []
     print(f"{file_path}/docs.txt")
     if exists(f"{file_path}/docs.txt"):
         f = open(f"{file_path}/docs.txt", "r")
@@ -20,38 +21,57 @@ def read_files(file_path):
         sys.exit("That directory or the queries.txt file does not exist.")
 
     for line in f:
-        file.append([s.strip() for s in line.split(" ")])
+        file.append([s.strip() for s in re.split(" |\t", line)])
 
-    query = [s.strip() for s in q.readline().split(" ")]
+    for line in q:
+        queries.append([s.strip() for s in line.split(" ")])
 
     f.close()
     q.close()
 
-    return file, query
+    return file, queries
 
 def make_dict(file):
     dict = {}
-    for line in file:
+    for i, line in enumerate(file):
         for word in line:
             if word in dict:
                 pass
             else: 
-                dict[word] = []
+                dict[word] = [0] * len(file)
+
+            dict[word][i] += 1
     return dict
 
 def print_no_words(dict):
     print(f"Words in dictionary: {len(dict)}")
 
-def print_query_details():
-    print("Query: {'QUERY'}")
-    print("Relevant documents: {'RELEVANT_DOCUMENTS'}")
+def print_query_details(dict, query, no_documents):
+    print(f"Query: {' '.join(query)}")
+
+    # Find query words that are actually in the file
+    relevant_query = []
+    for word in query:
+        if word in dict:
+            relevant_query.append(word)
+
+    # Find relevant documents
+    relevant_docs = list(range(1, no_documents + 1))
+    for word in relevant_query:
+        for i in range(0, no_documents):
+            if dict[word][i] == 0:
+                if (i + 1) in relevant_docs:
+                    relevant_docs.remove(i + 1)
+    print(f"Relevant documents: {' '.join([str(s) for s in relevant_docs])}")
     pass
 
 def main():
-    file, query = read_files(read_file_path())
+    # file, queries = read_files(read_file_path())
+    file, queries = read_files("testCases/set2")
     dict = make_dict(file)
     print_no_words(dict)
-    print(dict)
+    for query in queries:
+        print_query_details(dict, query, len(file))
 
 if __name__ == "__main__":
     main()
